c96c3fc4b971006a66f21e6a7dcffeb439f2a877
IzanDuesoLopez <izandueso@gmail.com>
2022-09-10 10:21:48 +0200

    DeployFinal

---
+++ Problema2/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Problema2';
+  nombre:any;
+  email:any;
+  mensaje:any;
+  resultado:any;
+
+  num_a:any;
+  num_b:any;
+  respuesta:any;
+
+  ngOnInit(){
+    this.num_a = Math.random() * (10 - 1) + 1;
+    this.num_b = Math.random() * (10 - 1) + 1;
+    this.num_a = parseInt(this.num_a);
+    this.num_b = parseInt(this.num_b);
+    this.respuesta = this.num_a + " + " + this.num_b;
+  }
+
+  comprobar(value:any){
+    this.resultado = this.num_a + this.num_b;
+    if(this.resultado === value){
+
+    } else {
+
+    }
+  }
+
+}
